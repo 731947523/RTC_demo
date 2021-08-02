2ae4bd6683b57612954e75362219b4f3e44457d4
xiaoshuai.chen <xiaoshuai.chen@vhall.com>
2021-08-02 15:05:58 +0800

    pre-commit 第五次实验成

--- video_RTCPeerConnection/Untitled-1.js
+++ video_RTCPeerConnection/Untitled-1.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 // Set up media stream constant and parameters.
 
@@ -6,17 +6,17 @@
 // Audio will not be streamed because it is set to "audio: false" by default.
 
 // Define initial start time of the call (defined as connection between peers).
-let startTime = null;
+let startTime = null
 
 // Define peer connections, streams and video elements.
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
+const localVideo = document.getElementById('localVideo')
+const remoteVideo = document.getElementById('remoteVideo')
 
-let localStream;
-let remoteStream;
+let localStream
+let remoteStream
 
-let localPeerConnection;
-let remotePeerConnection;
+let localPeerConnection
+let remotePeerConnection
 
 
 
@@ -25,90 +25,90 @@ let remotePeerConnection;
 
 // Connects with new peer candidate.
 function handleConnection(event) {
-  const peerConnection = event.target;
-  const iceCandidate = event.candidate;
+  const peerConnection = event.target
+  const iceCandidate = event.candidate
 
   if (iceCandidate) {
-    const newIceCandidate = new RTCIceCandidate(iceCandidate);
-    const otherPeer = getOtherPeer(peerConnection);
+    const newIceCandidate = new RTCIceCandidate(iceCandidate)
+    const otherPeer = getOtherPeer(peerConnection)
 
     otherPeer.addIceCandidate(newIceCandidate)
       .then(() => {
-        handleConnectionSuccess(peerConnection);
+        handleConnectionSuccess(peerConnection)
       }).catch((error) => {
-        handleConnectionFailure(peerConnection, error);
-      });
+        handleConnectionFailure(peerConnection, error)
+      })
 
     trace(`${getPeerName(peerConnection)} ICE candidate:\n` +
-          `${event.candidate.candidate}.`);
+          `${event.candidate.candidate}.`)
   }
 }
 
 // Logs that the connection succeeded.
 function handleConnectionSuccess(peerConnection) {
-  trace(`${getPeerName(peerConnection)} addIceCandidate success.`);
+  trace(`${getPeerName(peerConnection)} addIceCandidate success.`)
 };
 
 // Logs that the connection failed.
 function handleConnectionFailure(peerConnection, error) {
   trace(`${getPeerName(peerConnection)} failed to add ICE Candidate:\n`+
-        `${error.toString()}.`);
+        `${error.toString()}.`)
 }
 
 // Logs changes to the connection state.
 function handleConnectionChange(event) {
-  const peerConnection = event.target;
-  console.log('ICE state change event: ', event);
+  const peerConnection = event.target
+  console.log('ICE state change event: ', event)
   trace(`${getPeerName(peerConnection)} ICE state: ` +
-        `${peerConnection.iceConnectionState}.`);
+        `${peerConnection.iceConnectionState}.`)
 }
 
 // Logs error when setting session description fails.
 function setSessionDescriptionError(error) {
-  trace(`Failed to create session description: ${error.toString()}.`);
+  trace(`Failed to create session description: ${error.toString()}.`)
 }
 
 // Logs success when setting session description.
 function setDescriptionSuccess(peerConnection, functionName) {
-  const peerName = getPeerName(peerConnection);
-  trace(`${peerName} ${functionName} complete.`);
+  const peerName = getPeerName(peerConnection)
+  trace(`${peerName} ${functionName} complete.`)
 }
 
 // Logs success when localDescription is set.
 function setLocalDescriptionSuccess(peerConnection) {
-  setDescriptionSuccess(peerConnection, 'setLocalDescription');
+  setDescriptionSuccess(peerConnection, 'setLocalDescription')
 }
 
 // Logs success when remoteDescription is set.
 function setRemoteDescriptionSuccess(peerConnection) {
-  setDescriptionSuccess(peerConnection, 'setRemoteDescription');
+  setDescriptionSuccess(peerConnection, 'setRemoteDescription')
 }
 
 
 
 
 // Add click event handlers for buttons.
-callButton.addEventListener('click', callAction);
+callButton.addEventListener('click', callAction)
 
 
 
 // Gets the "other" peer connection.
 function getOtherPeer(peerConnection) {
   return (peerConnection === localPeerConnection) ?
-      remotePeerConnection : localPeerConnection;
+      remotePeerConnection : localPeerConnection
 }
 
 // Gets the name of a certain peer connection.
 function getPeerName(peerConnection) {
   return (peerConnection === localPeerConnection) ?
-      'localPeerConnection' : 'remotePeerConnection';
+      'localPeerConnection' : 'remotePeerConnection'
 }
 
 // Logs an action (text) and the time when it happened on the console.
 function trace(text) {
-  text = text.trim();
-  const now = (window.performance.now() / 1000).toFixed(3);
+  text = text.trim()
+  const now = (window.performance.now() / 1000).toFixed(3)
 
-  console.log(now, text);
+  console.log(now, text)
 }
 
