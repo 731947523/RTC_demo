b4cd357686323f5f17ce2b75dd14a3c2452c0ceb
xiaoshuai.chen <xiaoshuai.chen@vhall.com>
2021-09-06 22:37:45 +0800

    testt

--- server/index.js
+++ server/index.js
@@ -1,34 +1,26 @@
-let https = require('https')
-let http  = require('http')
-let fs = require('fs')
-let path = require('path')
-
-let Koa = require('koa')
-let router = require('koa-route')
-const serve = require('koa-static')
-let express = require('express')
-let serverIndex = require('serve-index')
-
-const app = new Koa()
-// let _mkdir = 'MediaRecord'
-// let _path = path.join(path.resolve(__dirname) +'/../' + _mkdir)
-let _path = 'D:/Downloads/API-Examples-Web-main/API-Examples-Web-main/Demo'
-console.log(_path)
-console.log('路径1----', path.resolve(__dirname))
-const main = serve(_path)
-app.use(main)
-
-app.use(router.get('/', ctx => {
-    ctx.response.type = 'html'
-    ctx.response.body = fs.createReadStream(_path+'/index.html')
-}))
-app.listen(3000)
+const https = require('https')
+const http  = require('http')
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const serverIndex = require('serve-index')
 
 
+const app = new express()
+const staticPath = './../'
+let endPath = path.join(__dirname, staticPath)
+app.use(serverIndex(endPath))
+app.use(express.static(endPath))
+
+
+
+// http server
+const http_server = http.createServer(app)
+http_server.listen(3000)
 
 // https server
 let options = {
-    key: fs.readFileSync(path.resolve() +'/ssl/privatekey.pem'),
-    cert: fs.readFileSync(path.resolve() +'/ssl/certificate.pem')
+    key: fs.readFileSync(endPath +'/ssl/private.pem'),
+    cert: fs.readFileSync(endPath +'/ssl/file.crt')
 }
-https.createServer(options, app.callback()).listen(443)
+https.createServer(options, app).listen(3001)
