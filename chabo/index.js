965da45a788947409fa2d4b98426af6a323dacfd
xiaoshuai.chen <xiaoshuai.chen@vhall.com>
2021-09-15 10:20:54 +0800

    add

--- chabo/index.js
+++ chabo/index.js
@@ -1,30 +1,30 @@
 window.onload = function(){
     const videoPlayer = document.getElementById('videoPlayer')
     window.videoPlayer = videoPlayer
-    const chrome88Canvas = document.createElement('canvas')
-    const chrome88canvasContext = chrome88Canvas.getContext('2d')
-    videoPlayer.addEventListener('play', ()=>{
-            chrome88Canvas.width = videoPlayer.videoWidth
-            chrome88Canvas.height = videoPlayer.videoHeight
-            drawVideoCanvas()
-            // 从canvas中抓取MediaStream
-            const canvasStream = chrome88Canvas.captureStream()
-            // 从video播放器中抓取MediaStream
-            const vodCpatureStream = videoPlayer.captureStream()
-            // audio track 从[video播放器抓取MediaStream对象]中获取
-            const audioTrack = vodCpatureStream.getAudioTracks()[0]
-            // video track 从[canvas抓取MediaStream对象]中获取
-            const videoTrack = canvasStream.getVideoTracks()[0]
-            console.log('audioTrack--', audioTrack)
-    })
+    // const chrome88Canvas = document.createElement('canvas')
+    // const chrome88canvasContext = chrome88Canvas.getContext('2d')
+    // videoPlayer.addEventListener('play', ()=>{
+    //         chrome88Canvas.width = videoPlayer.videoWidth
+    //         chrome88Canvas.height = videoPlayer.videoHeight
+    //         drawVideoCanvas()
+    //         // 从canvas中抓取MediaStream
+    //         const canvasStream = chrome88Canvas.captureStream()
+    //         // 从video播放器中抓取MediaStream
+    //         const vodCpatureStream = videoPlayer.captureStream()
+    //         // audio track 从[video播放器抓取MediaStream对象]中获取
+    //         const audioTrack = vodCpatureStream.getAudioTracks()[0]
+    //         // video track 从[canvas抓取MediaStream对象]中获取
+    //         const videoTrack = canvasStream.getVideoTracks()[0]
+    //         console.log('audioTrack--', audioTrack)
+    // })
     //将video播放器的画面绘制至canvas上
-    function drawVideoCanvas() {
-        setInterval(()=>{
-            chrome88canvasContext.drawImage(videoPlayer, 0, 0, chrome88Canvas.width, chrome88Canvas.height)
-        }, 40)
+    // function drawVideoCanvas() {
+    //     setInterval(()=>{
+    //         chrome88canvasContext.drawImage(videoPlayer, 0, 0, chrome88Canvas.width, chrome88Canvas.height)
+    //     }, 40)
     //window.requestAnimationFrame(drawVideoCanvas);
     // 备选方案：大概以 25 fps 渲染到 canvas 中
-    }
+    // }
 
 
     
--- test_sample/index.js
+++ test_sample/index.js
@@ -1,40 +1,5 @@
-class CreateInstance{
-    constructor(){
-        this.initCount = 19
-    }
-    createClient(opt, success = (params) => { }, failed = (params) => {}){
-        let {name, age} = opt
-        let sex = '男'
-        success({name, age, sex})
-    }
-    init (opt){
-        let {name, age} = opt
-        let sex = '男'
-        let _obj = {
-            name,
-            age,
-            sex
-        }
-        return new Promise((resolve, reject)=>{
-            if(this.initCount > 7){
-                reject(_obj)
-            }else{
-                resolve( _obj)
-            }
-        })
-    }
-}
 
-let test_ = new CreateInstance()
-test_.createClient({name: '晨曦', age: 27, like: '打游戏'}, function(params) {
-    console.log('createClient------success', params);
-})
 
-test_.init({name: '晨曦', age: 27, like: '打游戏'}).then(res=>{
-    console.log('初始化成功---', res);
-}).catch(err=>{
-    console.error('初始化失败', err);
-})
 
 
 
